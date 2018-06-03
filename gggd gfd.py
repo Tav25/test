0b55f8f034b1936391d6ab861cae1c161fbfc0b1
Tav25 <turbin28@gmail.com>
2018-06-03 21:51:09 +0300

    prosto test

--- gggd gfd.py
+++ gggd gfd.py
@@ -1,6 +1,37 @@
-import os
+from datetime import datetime
 
-os.system('dir')
-x = 'fsf f'
-#yfdhd g g
-x = 565
+
+
+
+
+def timeit(func):
+    def wrap():
+        start = datetime.now()
+        result = func()
+        print(datetime.now() - start)
+        return result
+    return wrap
+
+
+
+@timeit
+def one():
+    l = []
+    for x in  range(10 ** 4):
+        if x % 2 == 0:
+            l.append(x)
+
+    #return l
+@timeit
+def tho():
+    l = [x for x in range(10 ** 4) if x % 2 == 0]
+
+
+
+
+
+timeit(one())
+#print(l1)
+#print(l2)
+
+#input()
