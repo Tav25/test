cbe8038cd93f80893ce1feb4cb51e571024a4731
Tav25 <turbin28@gmail.com>
2020-12-12 23:28:33 +0300

    Revert "Merge pull request #2 from Tav25/dataFromApi"
    This reverts commit 6cbee04f12c48f875d05eadca7208e81535e6094, reversing
    changes made to 6e107b4d1486481fb2b78955e0732a6cba0937c1.

---
+++ gggd gfd.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+def timeit(func):
+    def wrap():
+        start = datetime.now()
+        result = func()
+        print(datetime.now() - start)
+        return result
+    return wrap
+
+
+
+@timeit
+def one():
+    l = []
+    for x in  range(10 ** 4):
+        if x % 2 == 0:
+            l.append(x)
+
+    #return l
+@timeit
+def tho():
+    l = [x for x in range(10 ** 4) if x % 2 == 0]
+
+
+
+
+
+timeit(one())
+# klklklklklk
