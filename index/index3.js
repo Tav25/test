a9215f7f369be0330c10ff09a7c26b611e8e0de3
Tav25 <turbin28@gmail.com>
2020-12-18 03:57:20 +0300

    index3

---
+++ index/index3.js
@@ -0,0 +1,124 @@
+(async () => {
+  let contrCovid;
+  let globalCovid;
+  let dateCovidData;
+  // let contrCovidfromDate;
+
+  await fetch('https://disease.sh/v3/covid-19/all')
+    .then((response) => response.json())
+    .then((entries) => {
+      // console.log(entries)
+      globalCovid = entries;
+    });
+
+  await fetch('https://disease.sh/v3/covid-19/countries')
+    .then((response) => response.json())
+    .then((entries) => {
+      // console.log(entries)
+      contrCovid = entries;
+    });
+  await console.log('1-hgghghh');
+  await fetch('https://disease.sh/v3/covid-19/historical?lastdays=all')
+    .then((response) => response.json())
+    .then((entries) => {
+      console.log(entries);
+      dateCovidData = entries;
+    })
+    .then(() => {
+      dateCovidData.forEach((element) => {
+        console.log(element);
+        for (let i = 0; i < 5; i++) {
+          console.log(i);
+        }
+      });
+    });
+
+  await console.log('2-hgghghh');
+  await main(globalCovid, contrCovid, dateCovidData);
+})();
+
+class Covid19 {
+  constructor(globalCovid, contrCovid, dateCovidData) {
+    // this.urlCountry = 'https://disease.sh/v3/covid-19/countries';
+    // this.urlTotal = 'https://disease.sh/v3/covid-19/all';
+    this.dataGlobalCovid = globalCovid;
+    this.datacontrCovid = contrCovid;
+    this.dataDateCovid = dateCovidData;
+
+    this.apiDataCountry = [];
+    this.apiDataTotal = [];
+    this.per100кРopulation = 100000;
+    this.dataForTest = 'testData';
+    this.getDataFromCovodApiTotal();
+    this.getDataFromCovodApiCountry();
+  }
+
+  getDataFromCovodApiCountry() {
+    // let entries = this.dataForTest;
+
+    this.datacontrCovid.forEach((element) => {
+      this.apiDataCountry.push(
+        {
+          country: element.country,
+          iso3: element.countryInfo.iso3,
+          flag: element.countryInfo.flag,
+          countryСenterСoordinates: [element.countryInfo.lat, element.countryInfo.long],
+          totalCases: element.cases,
+          totalDeaths: element.deaths,
+          totalRecovery: element.recovered,
+          casesInLastDay: element.todayCases,
+          deathInLastDay: element.todayDeaths,
+          recoveryInLastDay: element.todayRecovered,
+          per100KCases: Math.round((element.cases / element.population) * this.per100кРopulation),
+          per100KDeath: Math.round((element.deaths / element.population) * this.per100кРopulation),
+          per100KRecovery: Math.round((element.recovered / element.population) * this.per100кРopulation),
+          per100KCasesInLastDay: Math.round((element.todayCases / element.population) * this.per100кРopulation),
+          per100KDeathInLastDay: Math.round((element.todayDeaths / element.population) * this.per100кРopulation),
+          per100KRecoveryInLastDay: Math.round((element.todayRecovered / element.population) * this.per100кРopulation),
+          history: [0, 1, 2],
+
+        },
+      );
+    });
+  }
+
+  getDataFromCovodApiTotal() {
+    const entries = this.dataGlobalCovid;
+    this.apiDataTotal.push(
+      {
+        country: 'total',
+        totalCases: entries.cases,
+        totalDeaths: entries.deaths,
+        totalRecovery: entries.recovered,
+        casesInLastDay: entries.todayCases,
+        deathInLastDay: entries.todayDeaths,
+        recoveryInLastDay: entries.todayRecovered,
+        per100KCases: Math.round((entries.cases / entries.population) * this.per100кРopulation),
+        per100KDeath: Math.round((entries.deaths / entries.population) * this.per100кРopulation),
+        per100KRecovery: Math.round((entries.recovered / entries.population) * this.per100кРopulation),
+        per100KCasesInLastDay: Math.round((entries.todayCases / entries.population) * this.per100кРopulation),
+        per100KDeathInLastDay: Math.round((entries.todayDeaths / entries.population) * this.per100кРopulation),
+        per100KRecoveryInLastDay: Math.round((entries.todayRecovered / entries.population) * this.per100кРopulation),
+
+      },
+    );
+  }
+
+  get getCountriesData() {
+    return this.apiDataCountry;
+  }
+
+  get getTotalData() {
+    return this.apiDataTotal;
+  }
+
+  get getTestData() {
+    return this.apiDataTotal;
+  }
+}
+
+function main(globalCovid, contrCovid, dateCovidData) {
+  testCovid = new Covid19(globalCovid, contrCovid, dateCovidData);
+  // console.log(testCovid.getTestData);
+  // console.log(testCovid.getCountriesData);
+}
---
+++ index/modelMethods.js
@@ -0,0 +1,57 @@
+getCountries(index) {
+  // тут код, который выдирает из базы данных страну и её данные по показателю (index)
+  const result = [ // массив с объектами стран
+    {
+      countryID, // не уверен в необходимости countyID, возможно, можно только по названию
+      countryName,
+      countryFlag,
+      countryCoordinates: [lat, long],
+      countryIndex,
+    },
+    {
+      countryName,
+      // ...
+      countryIndex,
+    },
+    //...
+    //...
+    //...
+    {
+      countryName,
+      // ...
+      countryIndex,
+    },
+  ];
+  return result;
+}
+
+getCountry(country, index) {
+  // тут код, который выдирает из базы данных страну с указанным показателем
+  const result = {
+    countryID,
+    countryName,
+    countryFlag,
+    countryIndexes: {
+      cases,
+      deaths,
+      recovered,
+      dailyCases,
+      dailyDeaths,
+      // и т.д.
+      // 12 показателей
+    },
+    countryIndexHistory: [
+      {
+        date: 'dd/mm/yy';
+        index,
+      },
+      // показатель на каждый день
+    ]
+  };
+  return result;
+}
+
+getDate() {
+  // тут код, выдирающий последнюю дату, за которую есть данные по всем странам
+  return date; // 'dd/mm/yy hh:mm'
+}
